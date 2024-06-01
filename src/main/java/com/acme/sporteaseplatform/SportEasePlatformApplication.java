dc6221e19b65a4d0834e6aae4a3ccbcf8928d1d3
Mauricio Chacon Martinez <mauriciochaconmartinez05@gmail.com>
2024-06-01 17:44:57 -0500

    chore: create default project

---
+++ src/main/java/com/acme/sporteaseplatform/SportEasePlatformApplication.java
@@ -0,0 +1,13 @@
+package com.acme.sporteaseplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportEasePlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportEasePlatformApplication.class, args);
+	}
+
+}

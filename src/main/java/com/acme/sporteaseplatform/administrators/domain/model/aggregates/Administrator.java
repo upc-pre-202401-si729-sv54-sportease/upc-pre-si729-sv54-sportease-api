da0a2cfe599c19670aae692e2e71d2d7616cb24a
pierotm <pierotarazona822@gmail.com>
2024-06-22 08:29:12 -0500

    fix: entity administrator

--- src/main/java/com/acme/sporteaseplatform/administrators/domain/model/aggregates/Administrator.java
+++ src/main/java/com/acme/sporteaseplatform/administrators/domain/model/aggregates/Administrator.java
@@ -2,7 +2,13 @@ package com.acme.sporteaseplatform.administrators.domain.model.aggregates;
 
 import com.acme.sporteaseplatform.administrators.domain.model.commands.CreateAdministratorCommand;
 import com.acme.sporteaseplatform.shared.domain.model.aggregates.PersonAbstractAggregateRoot;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
 
+@Setter
+@Getter
+@Entity
 public class Administrator extends PersonAbstractAggregateRoot<Administrator> {
 
     protected Administrator(){}
--- src/main/java/com/acme/sporteaseplatform/administrators/infraestructure/persistence/jpa/repositories/AdministratorRepository.java
+++ src/main/java/com/acme/sporteaseplatform/administrators/infraestructure/persistence/jpa/repositories/AdministratorRepository.java
@@ -2,9 +2,11 @@ package com.acme.sporteaseplatform.administrators.infraestructure.persistence.jp
 
 import com.acme.sporteaseplatform.administrators.domain.model.aggregates.Administrator;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface AdministratorRepository  extends JpaRepository<Administrator, Long> {
     Optional<Administrator> findByEmail(String emailAddress);
 }

30806ff9c14ddb959b1e049d3fbd176dd0ea0966
pierotm <164135772+pierotm@users.noreply.github.com>
2024-06-25 19:47:16 -0500

    feat: add corsconfig in sporteasePlatformApplication

--- src/main/java/com/acme/sporteaseplatform/SportEasePlatformApplication.java
+++ src/main/java/com/acme/sporteaseplatform/SportEasePlatformApplication.java
@@ -2,12 +2,27 @@ package com.acme.sporteaseplatform;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class SportEasePlatformApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SportEasePlatformApplication.class, args);
+	}
+
+	@Configuration
+	public static class CorsConfig implements WebMvcConfigurer {
+		@Override
+		public void addCorsMappings(CorsRegistry registry) {
+			registry.addMapping("/**")
+					.allowedOrigins("*")
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+					.allowedHeaders("content-type")
+					.maxAge(3600);
+		}
 	}
 
 }
